65928d765e175fb075dee9e0cee6992436297593
ChaeYubin <cyoobin@naver.com>
2023-02-25 08:26:00 +0900

    💄 Home Styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,19 +5,23 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <Flex justify="center" align="center" pt="100px" direction="column">
-      <Text fontSize="3xl" textAlign="center">
-        Make My
-        <br />
-        Profile
+      <Text fontSize="5xl" textAlign="center">
+        <Text color="#90DC61CC">Make My</Text>
+        <Text color="#FEB2B2">Profile</Text>
       </Text>
-      <ButtonGroup mt="150px" gap="2">
-        <Link to="/">
-          <Button colorScheme="yellow">get other's</Button>
-        </Link>
-        <Link to="/make-my-profile">
-          <Button colorScheme="yellow">make my own</Button>
-        </Link>
-        <Link to="/edit"></Link>
+      <ButtonGroup mt="20px">
+        <Flex direction="column" alignItems="center">
+          <Link to="/make-my-profile">
+            <Button color="white" backgroundColor="#FEB2B2" mb="10px" w="280px">
+              make my own
+            </Button>
+          </Link>
+          <Link to="/">
+            <Button color="white" backgroundColor="#90DC61CC" w="280px">
+              get other's
+            </Button>
+          </Link>
+        </Flex>
       </ButtonGroup>
     </Flex>
   );
